103c19a26acb58a23ab83147a77094bc7295a0e4
tzdaniel9 <dzingozi@gmail.com>
2019-04-04 15:23:02 +0200

    keyword driven framework

---
+++ src/main/java/com/qa/hs/keyword/base/Base.java
@@ -0,0 +1,46 @@
+package com.qa.hs.keyword.base;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class Base {
+
+	public WebDriver driver;
+	public Properties prop;
+
+	public WebDriver init_driver(String browserName) {
+		if (browserName.equals("chrome")) {
+			System.setProperty("webdriver.chrome.driver",
+					"C:\\Users\\hp\\eclipse-workspace\\KeyWordDrivenHubSpot\\src\\drivers\\chromedriver.exe");
+			if (prop.getProperty("headless").equals("yes")) {
+				// headless mode
+				ChromeOptions options = new ChromeOptions();
+				options.addArguments("--headless");
+				driver = new ChromeDriver(options);
+			} else {
+				driver = new ChromeDriver();
+			}
+		}
+		return driver;
+	}
+
+	public Properties init_properties() {
+		prop = new Properties();
+		try {
+			FileInputStream ip = new FileInputStream("C:\\Users\\hp\\eclipse-workspace\\KeyWordDrivenHubSpot"
+					+ "\\src\\main\\java\\com\\qa\\hs\\keyword\\config\\config.properties");
+			prop.load(ip);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return prop;
+	}
+}
